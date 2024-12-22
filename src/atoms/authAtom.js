859a484a4c63fdc7c53f8883c167723959273e99
huynhduy <nlhd.2701@gmail.com>
2024-12-22 13:02:21 +0700

    fix auth

--- src/atoms/authAtom.js
+++ src/atoms/authAtom.js
@@ -1,7 +1,25 @@
-import {atom} from 'recoil'
-const authScreenAtom = atom({
-    key: 'authScreenAtom',
-    default: 'login',
+// import {atom} from 'recoil'
+// const authScreenAtom = atom({
+//     key: 'authScreenAtom',
+//     default: 'login',
   
-})
-export default authScreenAtom;
+// })
+// export default authScreenAtom;
+import { atom } from "recoil";
+
+// Quản lý trạng thái màn hình xác thực (login, signup)
+const authScreenAtom = atom({
+  key: "authScreenAtom",
+  default: "login", // Giá trị mặc định là login
+});
+
+// Quản lý trạng thái người dùng (đã đăng nhập hay chưa)
+const authStateAtom = atom({
+  key: "authStateAtom",
+  default: {
+    isAuthenticated: false, // Người dùng chưa đăng nhập
+    user: null, // Không có thông tin người dùng
+  },
+});
+
+export { authScreenAtom, authStateAtom };
